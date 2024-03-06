e6e80c048b22db95c64142836f0ca3b34d44190f
David Rm <155977317+drodmay1@users.noreply.github.com>
2024-03-06 10:23:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,19 @@ import palmerpenguins  # This package provides the Palmer Penguins dataset
 penguins_df = palmerpenguins.load_penguins()
 
 ui.page_opts(title="DavidRm Penguin", fillable=True)
-with ui.layout_columns():
+ui.h2("Side bar")
+with ui.sidebar():
+    ui.input_selectize(
+        "var", "Selected_attribute",
+        ["species", "island", "bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "sex"]
+    )
+    ui.input_numeric("bins", "Number of bins", 30)
 
-    @render_plotly
-    def plot1():
-        return px.histogram(px.data.tips(), y="tip")
 
+
+with ui.card(full_screen=True):
     @render_plotly
-    def plot2():
-        return px.histogram(px.data.tips(), y="total_bill")
+    def hist():
+        import plotly.express as px
+        from palmerpenguins import load_penguins
+        return px.histogram(load_penguins(), x=input.var(), nbins=input.bins())
