0d71fed287f4f0b6492936e5c595f81c26538e7b
David Rm <155977317+drodmay1@users.noreply.github.com>
2024-03-06 18:53:12 -0500

    Plotly scatterplot completed

--- app.py
+++ app.py
@@ -2,49 +2,61 @@ import plotly.express as px
 from shiny.express import input, ui
 from shinywidgets import render_plotly
 import palmerpenguins  # This package provides the Palmer Penguins dataset
+import pandas as pd
+import seaborn as sns
+from shiny import reactive, render, req
 
 # Use the built-in function to load the Palmer Penguins dataset
 penguins_df = palmerpenguins.load_penguins()
 
 ui.page_opts(title="DavidRm Penguin", fillable=True)
 
-ui.h2("Side bar")
-with ui.sidebar(open="open", bg="#f8f8f8"):
+with ui.sidebar(open="open"):
+    
+    ui.h2("Sidebar")
+    
     ui.input_selectize(
         "selected_attribute",
         "Select Plotly Attribute",
         ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
     )
 
-    ui.input_numeric("plotly_bin_count", "plotly_bin_count", 1, min=1, max=10)
+    ui.input_numeric("plotly_bin_count", "Number of Plotly bins", 30)
+    
+    ui.input_slider("seaborn_bin_count", "Number of Seaborn bins", 1, 100, 20)
 
-    (ui.input_slider("seaborn_bin_count", "seaborn_bin_count", 0, 20, 10),) 
-
-ui.input_checkbox_group(
-        "selected_species",
-        "Species in Scatterplot",
+    ui.hr()
+    
+    ui.input_checkbox_group(
+        "selected_species_list",
+        "Species",
         ["Adelie", "Gentoo", "Chinstrap"],
         selected=["Adelie"],
         inline=True,
     )
 
-ui.a("GitHub", href="https://github.com/drodmay1/cintel-02-data.git", target="_blank")
+    ui.a(
+        "GitHub",
+         href="https://github.com/drodmay1/cintel-02-data.git",
+         target="_blank",
+         )
+
+# Creates a Plotty Scatterplot showing all species
 
 with ui.card(full_screen=True):
-
     ui.card_header("Plotly Scatterplot: Species")
+
     @render_plotly
     def plotly_scatterplot():
-        return px.scatter(
-            penguins_df(),
+        return px.scatter(penguins_df,
             x="bill_length_mm",
             y="body_mass_g",
             color="species",
-            title="Penguins Plot (Plotly Express)",
+            title="Penguins Plot",
             labels={
                 "bill_length_mm": "Bill Length (mm)",
                 "body_mass_g": "Body Mass (g)",
             },
-            size_max=8, # set the maximum marker size
+            size_max=8, 
         )
     
